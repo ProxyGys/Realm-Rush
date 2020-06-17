08c2da3d57ea0a5ca30702eac9ebca09c1284d8f
Proxy_Gys <53296504+ProxyGys@users.noreply.github.com>
2020-06-17 10:01:52 +0200

    StartV2

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    [SerializeField] List<Waypoint> path;
+
+    void Start()
+    {
+        //StartCoroutine(FollowPath());
+    }
+
+    IEnumerator FollowPath()
+    {
+        print("Startin patrol ... ");
+        foreach (Waypoint waypoint in path)
+        {
+            transform.position = waypoint.transform.position;
+            print("Visiting" + waypoint);
+            yield return new WaitForSeconds(1f);
+        }
+        print("Ending patrol");
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Pathfinder.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinder : MonoBehaviour
+{
+    [SerializeField] Waypoint startWaypoint, endWaypoint;
+
+    Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+    Queue<Waypoint> queue = new Queue<Waypoint>();
+
+    [SerializeField] bool isRunning = true;
+    
+    Vector2Int[] directions =
+    {
+        Vector2Int.up,
+        Vector2Int.right,
+        Vector2Int.down,
+        Vector2Int.left
+    };
+
+    void Start()
+    {
+        LoadBlocks();
+        ColorStartAndEnd();
+        Pathfind();
+    }
+
+    void Pathfind()
+    {
+        queue.Enqueue(startWaypoint);
+
+        while(queue.Count > 0 && isRunning)
+        {
+            var searchCenter = queue.Dequeue();
+            print("S" + searchCenter);
+            HaltIfEndFound(searchCenter);
+            ExploreNeighbours(searchCenter);
+            searchCenter.isExplored = true;
+        }
+
+        print("F");
+    }
+
+    private void HaltIfEndFound(Waypoint serrchCenter)
+    {
+        if (serrchCenter == endWaypoint)
+        {
+            print("E");
+            isRunning = false;
+        }
+    }
+
+    private void ExploreNeighbours(Waypoint from)
+    {
+        if (!isRunning) { return; }
+
+        foreach (Vector2Int direction in directions)
+        {
+            Vector2Int neighbourCoordinates = from.GetGridPos() + direction;
+            try
+            {
+                QueueNewNeighbours(neighbourCoordinates);
+            }
+            catch
+            {
+                //do nothing
+            }
+        }
+
+    }
+
+    private void QueueNewNeighbours(Vector2Int neighbourCoordinates)
+    {
+        Waypoint neighbour = grid[neighbourCoordinates];
+        if (neighbour.isExplored)
+        {
+            // do nothing
+        }
+        else
+        {
+            neighbour.SetTopColor(Color.blue);
+            queue.Enqueue(neighbour);
+            print("Q" + neighbour);
+        }
+
+    }
+
+    private void ColorStartAndEnd()
+    {
+        startWaypoint.SetTopColor(Color.green);
+        endWaypoint.SetTopColor(Color.red);
+    }
+
+    private void LoadBlocks()
+    {
+        var waypoints = FindObjectsOfType<Waypoint>();
+        foreach (Waypoint waypoint in waypoints)
+        {
+            var gridPos = waypoint.GetGridPos();
+            if (grid.ContainsKey(gridPos))
+            {
+                Debug.LogWarning("skip" + waypoint);
+            }
+            else
+            {
+                grid.Add(gridPos, waypoint);
+            }
+        }
+    }
+
+}
