bf4f214453a5f8b5c9920381391f13eaf87bcd81
Proxy_Gys <53296504+ProxyGys@users.noreply.github.com>
2020-06-18 17:25:17 +0200

    Tower Chenge

---
+++ Assets/Scripts/TowerFactory.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerFactory : MonoBehaviour
+{
+    [SerializeField] Tower towerPrefab;
+    [SerializeField] int towerLimit = 5;
+
+    public Waypoint baseWaypoint;
+
+    Queue<Tower> towerQueue = new Queue<Tower>();
+
+    public void AddTower(Waypoint baseWaypoint)
+    {
+        int numTowers = towerQueue.Count;
+        
+        if (numTowers < towerLimit)
+        {
+            InstantiateNewTower(baseWaypoint);
+        }
+        else
+        {
+            MoveExistingTower();
+        }
+
+    }
+
+    private void InstantiateNewTower(Waypoint baseWaypoint)
+    {
+        var newTower = Instantiate(towerPrefab, baseWaypoint.transform.position, Quaternion.identity);
+        baseWaypoint.isPlaceable = false;
+
+
+        towerQueue.Enqueue(newTower);
+    }
+    private void MoveExistingTower()
+    {
+        var oltTower = towerQueue.Dequeue();
+
+        towerQueue.Enqueue(oltTower);
+    }
+
+
+}
--- Assets/Scripts/Waypoint.cs
+++ Assets/Scripts/Waypoint.cs
@@ -8,8 +8,6 @@ public class Waypoint : MonoBehaviour
     public bool isExplored = false;
     public Waypoint expoloredFrom;
     public bool isPlaceable = true;
-
-    [SerializeField] Tower towerPrefab;
     
     Vector2Int gridPos;
     
@@ -33,8 +31,7 @@ public class Waypoint : MonoBehaviour
         {
             if (isPlaceable)
             {
-                Instantiate(towerPrefab, transform.position, Quaternion.identity);
-                isPlaceable = false;
+                FindObjectOfType<TowerFactory>().AddTower(this);
             }
             else
             {
